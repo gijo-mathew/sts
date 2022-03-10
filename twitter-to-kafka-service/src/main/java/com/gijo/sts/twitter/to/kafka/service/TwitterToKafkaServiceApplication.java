77f71adffc67f694330415e19ce2a5a977f7b210
gijomathew <gijomathewakm@gmail.com>
2022-03-10 19:07:41 +0530

    initial microservice setup

---
+++ twitter-to-kafka-service/src/main/java/com/gijo/sts/twitter/to/kafka/service/TwitterToKafkaServiceApplication.java
@@ -0,0 +1,11 @@
+package com.gijo.sts.twitter.to.kafka.service;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitterToKafkaServiceApplication {
+
+    public static void main(String[] ars){
+
+    }
+}

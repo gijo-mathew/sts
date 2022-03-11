e8321226bfb8fcf48387fd379037ec779471984c
gijomathew <gijomathewakm@gmail.com>
2022-03-11 11:36:43 +0530

    twitter keywords configs

--- twitter-to-kafka-service/src/main/java/com/gijo/sts/twitter/to/kafka/service/TwitterToKafkaServiceApplication.java
+++ twitter-to-kafka-service/src/main/java/com/gijo/sts/twitter/to/kafka/service/TwitterToKafkaServiceApplication.java
@@ -1,11 +1,31 @@
 package com.gijo.sts.twitter.to.kafka.service;
 
+import com.gijo.sts.twitter.to.kafka.service.config.AppConfig;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
-public class TwitterToKafkaServiceApplication {
+public class TwitterToKafkaServiceApplication implements CommandLineRunner {
 
-    public static void main(String[] ars){
+    private final AppConfig appConfig;
+
+    public TwitterToKafkaServiceApplication(AppConfig appConfig) {
+        this.appConfig = appConfig;
+    }
+
+    public static void main(String[] args){
+        SpringApplication.run(TwitterToKafkaServiceApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("Twitter to kafka service started");
+        for(String key:appConfig.getTwitterKeywords()){
+            log.info("keyword {}", key);
+        }
 
     }
 }
